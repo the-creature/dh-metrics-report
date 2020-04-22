3575932f9e380c9680a658cf877555561cdfce3a
Liccy <the-creature@users.noreply.github.com>
2020-04-22 01:47:29 -0400

    img

--- lhci.sh
+++ lhci.sh
@@ -7,5 +7,5 @@ export LHCI_BUILD_CONTEXT__COMMIT_TIME=$(date +%Y-%m-%dT%H:%M:%S%z)
 export LHCI_BUILD_CONTEXT__CURRENT_BRANCH=master
 export LHCI_BUILD_CONTEXT__COMMIT_MESSAGE="Periodic run"
 export LHCI_BUILD_CONTEXT__AUTHOR="Liccy Fuentes <liccy.fuentes@daily-harvest.com>"
-export LHCI_BUILD_CONTEXT__AVATAR_URL="https://lh3.googleusercontent.com/-cGaW4g3_mNA/AAAAAAAAAAI/AAAAAAAAAAA/AMZuucnAeYugGyGDLcBVua-iOVSnIRpIjw/photo.jpg?sz=46"
+export LHCI_BUILD_CONTEXT__AVATAR_URL="https://picsum.photos/id/870/200/200"
 lhci autorun
