f6f183946b9bb0ed9e0ab65883377a4e9ca2744f
Liccy <the-creature@users.noreply.github.com>
2020-04-22 07:30:08 -0400

    LHCI_BUILD_CONTEXT__CURRENT_HASH

--- lhci.sh
+++ lhci.sh
@@ -2,7 +2,7 @@
 
 # https://explainshell.com/explain?cmd=set+-euxo%20pipefail
 set -euxo pipefail
-export LHCI_BUILD_CONTEXT__CURRENT_HASH=$(head -n 4096 /dev/urandom | openssl sha1 | sed 's/(stdin)=//g')
+export LHCI_BUILD_CONTEXT__CURRENT_HASH=$(head -n 4096 /dev/urandom | openssl sha1 | sed 's/(stdin)=//g' | fold -w 8 | head -n 1)
 export LHCI_BUILD_CONTEXT__COMMIT_TIME=$(date +%Y-%m-%dT%H:%M:%S%z)
 export LHCI_BUILD_CONTEXT__CURRENT_BRANCH=master
 export LHCI_BUILD_CONTEXT__COMMIT_MESSAGE="Periodic run"
